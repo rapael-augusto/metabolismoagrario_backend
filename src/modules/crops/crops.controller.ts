3b90941cd7bfe47a174019a620fc3445ac7fadbf
Gust4voSales <manoel0gustavo@gmail.com>
2024-04-24 17:55:25 -0300

    feat: change READ crop routes to public

--- src/modules/crops/crops.controller.ts
+++ src/modules/crops/crops.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CropsService } from './crops.service';
 import { CreateCropDto } from './dto/create-crop.dto';
+import { PublicRoute } from 'src/auth/decorators/public-route-decorator';
 
 @Controller('crops')
 export class CropsController {
@@ -11,11 +12,13 @@ export class CropsController {
     return await this.cropsService.create(createCropDto);
   }
 
+  @PublicRoute()
   @Get()
   async findAll() {
     return await this.cropsService.findAll();
   }
 
+  @PublicRoute()
   @Get(":id")
   async findOne(@Param("id") id: string) {
     return await this.cropsService.findOne(id);
