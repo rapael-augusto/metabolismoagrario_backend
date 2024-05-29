14f7ae1e9485922b34ae2f5651cfcdad5de04964
Gust4voSales <manoel0gustavo@gmail.com>
2024-05-28 21:02:00 -0300

    feat: include role on session return

--- src/modules/session/session.service.ts
+++ src/modules/session/session.service.ts
@@ -35,7 +35,8 @@ export class SessionService {
       user: {
         id: user.id,
         name: user.name,
-        email: user.email
+        email: user.email,
+        role: user.role,
       },
       accessToken,
       refreshToken,
