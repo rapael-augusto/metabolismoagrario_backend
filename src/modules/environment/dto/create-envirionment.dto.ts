205fa75d39d9252e73ef1f0f66c9894d1ba2c7bb
Igordcs <igorcostamdt@gmail.com>
2025-07-10 12:32:26 -0300

    fix: corrige campos 'não informado'

--- src/modules/environment/dto/create-envirionment.dto.ts
+++ src/modules/environment/dto/create-envirionment.dto.ts
@@ -3,34 +3,43 @@ import { IrrigationTypes } from '@prisma/client';
 import { SoilTypes } from '@prisma/client';
 import { CultivationSystem } from '@prisma/client';
 import { BiomeTypes, ClimatesTypes } from '@/types/index';
+import { Transform } from 'class-transformer';
 
 export class CreateEnvironmentDTO {
   @IsOptional()
   @IsEnum(ClimatesTypes)
+  @Transform(({ value }) => (value === '' ? null : value))
   climate?: ClimatesTypes;
 
   @IsOptional()
   @IsEnum(BiomeTypes)
+  @Transform(({ value }) => (value === '' ? null : value))
   biome?: BiomeTypes;
 
   @IsOptional()
   @IsString()
+  @Transform(({ value }) => (value === '' ? null : value))
   customBiome?: string;
 
   @IsOptional()
+  @Transform(({ value }) => (value === '' ? null : value))
   irrigation?: IrrigationTypes;
 
   @IsString()
   @IsNotEmpty()
+  @Transform(({ value }) => (value === '' ? null : value))
   country: string;
 
   @IsOptional()
+  @Transform(({ value }) => (value === '' ? null : value))
   soil?: SoilTypes;
 
   @IsOptional()
   @IsString()
+  @Transform(({ value }) => (value === '' ? null : value))
   customSoil?: string;
 
   @IsOptional()
+  @Transform(({ value }) => (value === '' ? null : value))
   cultivationSystem?: CultivationSystem;
 }
